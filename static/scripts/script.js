6844a8df3e2203e255b42036ca390ec9d8d6c04c
ankit723 <72332524+ankit723@users.noreply.github.com>
2023-05-12 18:32:54 +0530

    regsitration enhanced

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,14 +1,18 @@
 let input = document.getElementById("whatareyou");
+let select = document.getElementById("what_are_you");
 let companyname = document.getElementById('company_name');
 let universityname = document.getElementById('university_name');
 
 
 input.addEventListener("input", function() {
     if(input.value==="company"){
+        
         companyname.style.display='block';
         universityname.style.display='none';
+        select.style.display="none";
     }
     if(input.value==="university"){
+        select.style.display="none";
         companyname.style.display='none';
         universityname.style.display='block';
     }
